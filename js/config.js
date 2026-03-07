e83af3709f9f1e02f11fe461fa1c8590c93ae508
nutrilabhealth <nutrilabhealth.info@gmail.com>
2026-03-07 12:57:46 +0300

    Add configuration file for Supabase and local storage

---
+++ js/config.js
@@ -0,0 +1,7 @@
+export const SUPABASE_URL = "https://cthfqhxnplyibdsjzcrq.supabase.co";
+
+export const SUPABASE_KEY = "sb_publishable_AjGX1zKhV8kEyGMbvAKwQg_srda-oyI";
+
+export const LS_CART = "nutrilab_cart";
+
+export const LS_FAV = "nutrilab_fav";
