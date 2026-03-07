a6a4034fbe0c196151b2a70ac1af0ac4e6067349
nutrilabhealth <nutrilabhealth.info@gmail.com>
2026-03-07 15:58:00 +0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,21 +1,13 @@
-export function getCart(){
-
-return JSON.parse(localStorage.getItem("nutrilab_cart")||"[]")
-
+export function getCart() {
+  return JSON.parse(localStorage.getItem("nutrilab_cart") || "[]");
 }
 
-export function addToCart(product){
-
-let cart=getCart()
-
-cart.push(product)
-
-localStorage.setItem("nutrilab_cart",JSON.stringify(cart))
-
+export function addToCart(product) {
+  let cart = getCart();
+  cart.push(product);
+  localStorage.setItem("nutrilab_cart", JSON.stringify(cart));
 }
 
-export function getCartCount(){
-
-return getCart().length
-
+export function getCartCount() {
+  return getCart().length;
 }
